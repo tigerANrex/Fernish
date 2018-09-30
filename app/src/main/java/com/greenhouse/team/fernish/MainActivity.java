fb2ac28d6ee2b9caafb9b150269e78482de708a8
tigerANrex <nguyen11@memphis.edu>
2018-09-30 11:50:01 -0500

    Working ListView

--- app/src/main/java/com/greenhouse/team/fernish/MainActivity.java
+++ app/src/main/java/com/greenhouse/team/fernish/MainActivity.java
@@ -2,13 +2,26 @@ package com.greenhouse.team.fernish;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] plantsArray = {"Sago Palm", "Aloe Vera", "Peace Lily", "Succ-ulents", "Bamboo"};
+
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, plantsArray);
+
+        listView = (ListView) findViewById(R.id.mainMenu);
+        listView.setAdapter(adapter);
     }
 }
